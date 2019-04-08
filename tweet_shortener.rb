bb97616efb60e369e5d4e9f9920069b3d66d75d2
Renee Nordholm <reneenordholm@gmail.com>
2019-04-08 22:32:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,11 @@
 def dictionary
   dictionary = {
-    "hello" => "hi"
-    "to, two, too"  =>  "2"
-    "for, four"  =>  "4"
-    "be"  =>  "b"
-    "you"  =>  "u"
-    "at"  =>  "@"
+    "hello" => "hi",
+    "to, two, too"  =>  "2",
+    "for, four"  =>  "4",
+    "be"  =>  "b",
+    "you"  =>  "u",
+    "at"  =>  "@",
     "and"  =>  "&"
   }
 end
