4a888d815c9fe397212dc11d5db3ca32d90be4a4
Renee Nordholm <reneenordholm@gmail.com>
2019-04-08 22:58:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif phrase.length <= 140
+    else phrase.length <= 140
       phrase
     end
   end
